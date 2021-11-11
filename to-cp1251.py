8604defe1647b10ee2aa4761cf22111a805c93de
Mansur Mamirov <crazy_drummer@mail.ru>
2021-11-11 22:35:57 +0600

    Universalize for eny encoding.

--- to-cp1251.py
+++ to-cp1251.py
@@ -3,21 +3,25 @@ import codecs
 import chardet
 from shutil import copyfile 
 
-source_folder = 'C:\\Users\\Mans\\Documents\\Dinect\\20211110-fixed\\'
-target_folder = 'C:\\Users\\Mans\\Documents\\Dinect\\20211110-cp1251\\'
+source_folder = 'D:\\Temp\\0000-dinect\\20211111\\unconverted'
+target_folder = 'D:\\Temp\\0000-dinect\\20211111\\utf-8'
+target_encoding = 'utf-8'
+target_encoding_str = 'utf-8'
+# target_encoding = 'cp1251'
+# target_encoding_str = 'windows-1251'
 
 files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(source_folder) for f in filenames]
 
 count = 0
 nones = []
-cp1251s = []
+allready_in_target_encoding = []
 for source_filename in files:
 
 	rel_path = source_filename.replace(source_folder, '')
 	output_filename = target_folder + rel_path
 	output_dir = os.path.dirname(output_filename)
 	if not os.path.isdir(output_dir):
-		print("New dir: %s", output_dir)
+		print("New dir: %s" % output_dir)
 		os.makedirs(output_dir, exist_ok=True)
 
 	rawdata = open(source_filename, "rb").read()
@@ -25,11 +29,11 @@ for source_filename in files:
 
 	print("%d) %s: %s" % (count, encoding, source_filename))
 
-	if (not encoding is None) and (encoding.lower() != 'windows-1251'):
+	if (not encoding is None) and (encoding.lower() != target_encoding_str):
 
 		f = codecs.open(source_filename, 'r', encoding)
 		u = f.read()   # now the contents have been transformed to a Unicode string
-		out = codecs.open(output_filename, 'w', 'cp1251')
+		out = codecs.open(output_filename, 'w', target_encoding)
 		out.write(u)   # and now the contents have been output as UTF-8
 		count += 1
 
@@ -38,12 +42,12 @@ for source_filename in files:
 		nones.append(source_filename)
 		copyfile(source_filename, output_filename)
 
-	elif encoding.lower() == 'windows-1251':
+	elif encoding.lower() == target_encoding:
 		
-		cp1251s.append(source_filename)
+		allready_in_target_encoding.append(source_filename)
 		copyfile(source_filename, output_filename)
 
 
 print("Encoded: %s files" % count)
 print("Nones: %s \n%s " % (len(nones), "\n".join(nones)))
-print("cp1251s: %s \n%s" % (len(cp1251s), "\n".join(cp1251s)))
+print("allready_in_target_encoding: %s \n%s" % (len(allready_in_target_encoding), "\n".join(allready_in_target_encoding)))
